bb89476be58fd06e78b33615abb364400291ca78
Bartosz <golebiowski.bartosz.21@gmail.com>
2025-11-26 19:51:30 +0100

    db structure developed (categories, stones)

--- mineral-shop-backend/products/models.py
+++ mineral-shop-backend/products/models.py
@@ -1,9 +1,49 @@
 from django.db import models
 
-class Product(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
 
     def __str__(self):
         return self.name
+    
+class Stone(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="products"
+    )
+    stones = models.ManyToManyField(
+        Stone,
+        related_name="products",
+        blank=True
+    )
+    
+    def __str__(self):
+        return self.name
+    
+class Image(models.Model):
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="images"
+    )
+    upload = models.ImageField(
+        upload_to='products/',
+        null=True,
+        blank=True
+    )
--- mineral-shop-backend/products/serializers.py
+++ mineral-shop-backend/products/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Image
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    upload = serializers.ImageField(use_url=True)
+
+    class Meta:
+        model = Image
+        fields = ['id', 'upload']
 
 class ProductSerializer(serializers.ModelSerializer):
+    images = ImageSerializer(many=True, read_only=True)
+
     class Meta:
         model = Product
-        fields = ['id', 'name', 'price', 'image']
+        fields = ['id', 'name', 'price', 'images']
