4ff4c1a2e21f9d9a93dd977763668d9e3b1cf732
Bartosz <golebiowski.bartosz.21@gmail.com>
2026-01-30 21:22:18 +0100

    db fudge fixed

--- mineral-shop-backend/shop_backend/settings.py
+++ mineral-shop-backend/shop_backend/settings.py
@@ -89,10 +89,10 @@ WSGI_APPLICATION = 'shop_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
         'PORT': 5432,
     }
 }
