19e5ed348af1b92b6c5ff927a29ad86519931e27
Bartosz <golebiowski.bartosz.21@gmail.com>
2026-02-03 21:37:43 +0100

    csrf setting

--- mineral-shop-backend/shop_backend/settings.py
+++ mineral-shop-backend/shop_backend/settings.py
@@ -28,6 +28,8 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",")
 
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",")
+
 # Application definition
 
 INSTALLED_APPS = [
