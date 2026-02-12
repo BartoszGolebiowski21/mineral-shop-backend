59037e9f1f6fac1c49f0b90065c02c745e56e247
Bartosz <golebiowski.bartosz.21@gmail.com>
2026-02-12 14:33:33 +0100

    filtering products by categories

--- mineral-shop-backend/products/filters.py
+++ mineral-shop-backend/products/filters.py
@@ -5,13 +5,11 @@ from .models import Product
 class ProductFilter(django_filters.FilterSet):
     id__in = django_filters.BaseInFilter(field_name='id', lookup_expr='in')
     stones = django_filters.BaseInFilter(field_name='stones__slug', lookup_expr='in')
+    categories = django_filters.BaseInFilter(field_name='category__slug', lookup_expr='in')
 
     class Meta:
         model = Product
-        fields = ['id', 'id__in', 'stones']
+        fields = ['id']
 
     def filter_queryset(self, queryset):
-        queryset = super().filter_queryset(queryset)
-        if 'stones' in self.form.cleaned_data and self.form.cleaned_data['stones']:
-            return queryset.distinct()
-        return queryset
+        return super().filter_queryset(queryset).distinct()
